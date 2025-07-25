8b15f120110d8839b0a4aca8e81e3e2541037dbb
Yusuf <122007538+yusufoksuzoglu@users.noreply.github.com>
2025-07-25 23:46:54 +0300

    Create pass_accuracy_prediction.py

---
+++ pass_accuracy_prediction.py
@@ -0,0 +1,29 @@
+# Lineer modeller
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+
+# Karar ağacı bazlı
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
+
+# XGBoost ve LightGBM
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+
+# K-En Yakın Komşu
+from sklearn.neighbors import KNeighborsRegressor
+
+# Destek Vektör Regresyonu
+from sklearn.svm import SVR
+
+# Lineer modeller
+from sklearn.linear_model import LogisticRegression
+
+# XGBoost ve LightGBM
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+
+# K-En Yakın Komşu
+from sklearn.neighbors import KNeighborsClassifier
+
+# Destek Vektör Makinesi
+from sklearn.svm import SVC
