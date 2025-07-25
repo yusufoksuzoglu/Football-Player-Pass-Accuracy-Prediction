16aab637f9fc4eddfae58a3e6f53c0379d7383de
Yusuf <122007538+yusufoksuzoglu@users.noreply.github.com>
2025-07-25 23:40:14 +0300

    Create identities_of_footballers_data.py

---
+++ identities_of_footballers_data.py
@@ -0,0 +1,47 @@
+footballers_train = [
+    "Lionel Messi",
+    "Cristiano Ronaldo",
+    "Neymar Jr",
+    "Kevin De Bruyne",
+    "Virgil van Dijk",
+    "Manuel Neuer",
+    "Mohamed Salah",
+    "Luka Modrić",
+    "Sergio Ramos",
+    "Kylian Mbappé",
+    "Robert Lewandowski",
+    "N'Golo Kanté",
+    "Jan Oblak",
+    "Sadio Mané",
+    "Joshua Kimmich",
+    "Trent Alexander-Arnold",
+    "Harry Kane",
+    "Paul Pogba",
+    "Raheem Sterling",
+    "Ederson"
+]
+
+identity_of_footballers_train = {
+    "positions": [
+        "Forvet", "Forvet", "Forvet", "Orta Saha", "Defans", "Kaleci", "Forvet", "Orta Saha",
+        "Defans", "Forvet", "Forvet", "Orta Saha", "Kaleci", "Forvet", "Orta Saha", "Defans",
+        "Forvet", "Orta Saha", "Forvet", "Kaleci"
+    ],
+    "youth_academy": [
+        "Newell's Old Boys", "Sporting CP", "Santos", "Genk", "Willem II", "Schalke 04",
+        "El Mokawloon", "Dinamo Zagreb", "Sevilla", "AS Bondy", "Varsovia Warsaw",
+        "JS Suresnes", "Olimpija Ljubljana", "Génération Foot", "VfB Stuttgart", "Liverpool",
+        "Tottenham Hotspur", "Le Havre", "Queens Park Rangers", "São Paulo"
+    ],
+    "coaches": [
+        "Pep Guardiola", "Sir Alex Ferguson", "Tite", "Roberto Martínez", "Jürgen Klopp",
+        "Joachim Löw", "Jürgen Klopp", "Zlatko Dalić", "José Mourinho", "Didier Deschamps",
+        "Hans-Dieter Flick", "Didier Deschamps", "Diego Simeone", "Jürgen Klopp",
+        "Julian Nagelsmann", "Jürgen Klopp", "Gareth Southgate", "Didier Deschamps",
+        "Gareth Southgate", "Pep Guardiola"
+    ],
+    "team_strengths": [
+        95, 94, 90, 90, 89, 90, 89, 94, 94, 90,
+        90, 85, 86, 89, 90, 89, 83, 82, 90, 90
+    ]
+}
